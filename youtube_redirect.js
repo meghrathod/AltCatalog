e33c8c6ddd57b7cf0467896ab9b19a3da49a312c
Megh Rathod <msr.megh@gmail.com>
2021-10-09 20:39:00 +0530

    Trying changes

--- youtube_redirect.js
+++ youtube_redirect.js
@@ -1,1 +1,1 @@
-window.location.href = `youtube://${window.location.host}${window.location.pathname}`;
+window.location.href = `youtube://${window.location.host}${window.location.pathname}${window.location.search}
